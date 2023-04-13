243f47dd766a1d6c910ac6579b31fccf107aea51
Joao Mateus <jaomateus@gmail.com>
2023-04-13 18:35:14 +0100

    add smalSidebar links

--- src/components/SmallSidebar.js
+++ src/components/SmallSidebar.js
@@ -1,7 +1,55 @@
 import styled from "styled-components";
+import { FaTimes } from "react-icons/fa";
+import { NavLink } from "react-router-dom";
+import Logo from "./Logo";
+import { useSelector, useDispatch } from "react-redux";
+import { toggleSidebar } from "../features/user/userSlice";
+import links from "../utils/links";
 
 const SmallSidebar = () => {
-	return <Wrapper>SmallSidebar</Wrapper>;
+	const { isSidebarOpen } = useSelector((store) => store.user);
+	const dispatch = useDispatch();
+	const toggle = () => {
+		dispatch(toggleSidebar());
+	};
+
+	return (
+		<Wrapper>
+			<div
+				className={
+					isSidebarOpen ? "sidebar-container show-sidebar" : "sidebar-container"
+				}
+			>
+				<div className="content">
+					<button className="close-btn" onClick={toggle}>
+						<FaTimes />
+					</button>
+					<header>
+						<Logo />
+					</header>
+					<div className="nav-links">
+						{links.map((link) => {
+							const { text, path, id, icon } = link;
+							return (
+								<NavLink
+									to={path}
+									className={({ isActive }) =>
+										isActive ? "nav-link active" : "nav-link"
+									}
+									key={id}
+									onClick={toggle}
+									end
+								>
+									<span className='icon'>{icon}</span>
+									{text}
+								</NavLink>
+							);
+						})}
+					</div>
+				</div>
+			</div>
+		</Wrapper>
+	);
 };
 
 const Wrapper = styled.aside`
