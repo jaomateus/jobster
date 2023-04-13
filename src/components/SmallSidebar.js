d7c3c1c648d4fa2aba9d76de2f7bb0384791603d
Joao Mateus <jaomateus@gmail.com>
2023-04-13 18:41:46 +0100

    create navLinks component

--- src/components/SmallSidebar.js
+++ src/components/SmallSidebar.js
@@ -1,10 +1,9 @@
 import styled from "styled-components";
 import { FaTimes } from "react-icons/fa";
-import { NavLink } from "react-router-dom";
 import Logo from "./Logo";
 import { useSelector, useDispatch } from "react-redux";
 import { toggleSidebar } from "../features/user/userSlice";
-import links from "../utils/links";
+import NavLinks from "./NavLinks";
 
 const SmallSidebar = () => {
 	const { isSidebarOpen } = useSelector((store) => store.user);
@@ -26,26 +25,8 @@ const SmallSidebar = () => {
 					</button>
 					<header>
 						<Logo />
-					</header>
-					<div className="nav-links">
-						{links.map((link) => {
-							const { text, path, id, icon } = link;
-							return (
-								<NavLink
-									to={path}
-									className={({ isActive }) =>
-										isActive ? "nav-link active" : "nav-link"
-									}
-									key={id}
-									onClick={toggle}
-									end
-								>
-									<span className='icon'>{icon}</span>
-									{text}
-								</NavLink>
-							);
-						})}
-					</div>
+					</header> 
+					<NavLinks toggleSidebar={toggle}/>
 				</div>
 			</div>
 		</Wrapper>
