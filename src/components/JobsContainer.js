77e6d70341b3cbb6b7822965ebdae6c363e70ada
Joao Mateus <jaomateus@gmail.com>
2023-04-18 14:48:23 +0100

    create all jobs page

--- src/components/JobsContainer.js
+++ src/components/JobsContainer.js
@@ -3,10 +3,15 @@ import Job from "./Job";
 import styled from "styled-components";
 import { useDispatch, useSelector } from "react-redux";
 import Loading from "./Loading";
+import { getAllJobs } from "../features/allJobs/allJobsSlice";
 
 const JobsContainer = () => {
 	const { jobs, isLoading } = useSelector((store) => store.allJobs);
 	const dispatch = useDispatch();
+
+	useEffect(() => {
+		dispatch(getAllJobs());
+	}, []);
 
 	if (isLoading) {
 		return <Loading center />;
@@ -19,6 +24,7 @@ const JobsContainer = () => {
 			</Wrapper>
 		);
 	}
+
 
 	return (
 		<Wrapper>
--- src/features/allJobs/allJobsSlice.js
+++ src/features/allJobs/allJobsSlice.js
@@ -1,4 +1,6 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+import { toast } from "react-toastify";
+import customFetch from "../../utils/axios";
 
 
 const initialFiltersState = {
@@ -20,10 +22,41 @@ const initialState = {
 	...initialFiltersState,
 };
 
+export const getAllJobs = createAsyncThunk(
+	"allJobs/getJobs",
+	async (_, thunkAPI) => {
+		let url = '/jobs';
+
+		try {
+			const resp = await customFetch.get(url, {
+				headers: {
+					authorization: `Bearer ${thunkAPI.getState().user.user.token}`,
+				},
+			});
+
+			return resp.data;
+		} catch (error) {
+			return thunkAPI.rejectWithValue(error.response.data.msg);
+		}
+	},
+);
+
 const allJobsSlice = createSlice({
-    name: 'allJobs',
-    initialState,
-})
+	name: "allJobs",
+	initialState,
+	extraReducers: {
+		[getAllJobs.pending]: (state) => {
+			state.isLoading = true;
+		},
+		[getAllJobs.fulfilled]: (state, { payload }) => {
+			state.isLoading = false;
+			state.jobs = payload.jobs;
+		},
+		[getAllJobs.rejected]: (state, { payload }) => {
+			state.isLoading = false;
+			toast.error(payload);
+		},
+	},
+});
 
-
-export default allJobsSlice.reducer
+export default allJobsSlice.reducer;
